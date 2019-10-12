1bf40411848968f559381b94c0e49bb76b3b2512
Marian Q <mariansofia1999@hotmail.com>
2019-10-11 23:22:02 -0500

    Lista de Carritos
    Se añaden carritos y se visualizan

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //Aqui va todito
+
+var vehicles = [];
+
 app.get('/', (request, response) =>{
     response.sendFile(`${__dirname}/public/home.html`);
+});
+
+app.get('/api/vehicle', (request, response) =>{
+
+    response.send(vehicles);
+    console.log('Enviados')
+
+});
+
+app.post('/api/vehicle', (request, response) => {
+    console.log(request.body);
+    vehicles.push(request.body);
+    response.send({
+        message: 'ok',
+    });
 });
 
 
---
+++ public/scripts/home.js
@@ -0,0 +1,57 @@
+function handleLoad(){
+
+    var form = document.querySelector('.mainForm');
+
+//Refresh List
+
+function refreshList(){
+    fetch('/api/vehicle').then((raw) => {
+        return raw.json();
+    })
+    .then((inf) => {
+
+        var carContainer = document.querySelector('.container__cars');
+        carContainer.innerHTML = "";
+        inf.forEach((element) => {
+            var vehicle = document.createElement('div');
+            vehicle.classList.add('car');
+            vehicle.innerHTML = '<p class="car__brand">'+element.brand+'</p><p class="car__wheels">'+element.wheels+'</p><img src="/img/edit.svg" alt="" class="car__edit"><img src="/img/delete.svg" alt="" class="car__delete">';
+            carContainer.appendChild(vehicle);
+            vehicle.style.backgroundColor = element.color;
+            console.log('Agregado carrito');
+
+        });
+
+    });
+}
+
+//Send info
+form.addEventListener('submit', () => {
+
+    event.preventDefault();
+
+    var info = new FormData(form);
+
+    var data = new URLSearchParams(info);
+
+    var promise = fetch('api/vehicle', {
+        method: 'POST',
+        body: data
+    });
+
+    promise.then((raw) => {
+        return raw.json();
+    })
+    .then((inf) => {
+        form.reset();
+        refreshList();
+        console.log(inf);
+    })
+});
+
+
+
+
+}
+
+window.addEventListener('load', handleLoad);
